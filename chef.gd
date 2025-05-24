ec8dc33118601182e81566087bf85acaa3638c7a
b1gd3vd0g <devin.peevy@outlook.com>
2025-05-23 17:01:26 -0700

    Add a script for the chef (right now essentially just a comment)

---
+++ chef.gd
@@ -0,0 +1,6 @@
+# The Chef is the character which is controlled by the user.
+# The Chef moves around with the WASD/arrow keys.
+# The Chef can interact with various items in the kitchen in order to feed the
+# most Mouths.
+
+extends CharacterBody2D
