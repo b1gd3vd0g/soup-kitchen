184653a4ed0832eb1bc036ad54f2cba4a9e62bd5
b1gd3vd0g <devin.peevy@outlook.com>
2025-05-23 17:24:52 -0700

    Make chef start to  listen to user input and move. It is not done yet (and the animations still need made for horizontal movement).

--- chef.gd
+++ chef.gd
@@ -4,3 +4,29 @@
 # most Mouths.
 
 extends CharacterBody2D
+
+@export var speed = 200
+var screen_size
+
+func _ready():
+	screen_size = get_viewport().size()
+	hide()
+
+func _process(delta: float) -> void:
+	# Move the chef according to user input.
+	var chef_velocity: Vector2 = Vector2.ZERO
+	if Input.is_action_pressed("move_up"):
+		chef_velocity.y -= 1
+		$Sprite.animation = "walk_up"
+	if Input.is_action_pressed("move_down"):
+		chef_velocity.y += 1
+		$Sprite.animation = "walk_down"
+	if Input.is_action_pressed("move_left"):
+		chef_velocity.x -= 1
+	if Input.is_action_pressed("move_right"):
+		chef_velocity.x += 1
+	if chef_velocity.length() > 0:
+		chef_velocity = chef_velocity.normalized() * speed
+	position += velocity * delta
+	position = position.clamp(Vector2.ZERO, screen_size)
+	
