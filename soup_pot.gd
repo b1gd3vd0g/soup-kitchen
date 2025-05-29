9ba7c0fde881d692d4ee3806f4c65e581a15049f
Miguel Ramos <ramosmiguel40@gmail.com>
2025-05-28 23:34:42 -0700

    Created controller and main scene for Catch The Ingredients

--- soup_pot.gd
+++
@@ -1,7 +0,0 @@
-extends CharacterBody2D
-
-func _physics_process(_delta: float) -> void:
-	var velocity = get_global_mouse_position() - global_position
-	move_and_collide(velocity)
-	
-	move_and_slide()
